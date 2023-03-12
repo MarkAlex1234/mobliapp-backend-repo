94134aa88b8ce322029d3b360589b257383ca0ba
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-03-12 16:45:00 +1300

    Revert "chnages"
    This reverts commit 58ae8850b086fdedf2cc84b10b123ea345ef82f7.

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -21,13 +21,13 @@ let AppController = class AppController {
     }
 };
 __decorate([
-    (0, common_1.Get)('test'),
+    (0, common_1.Get)(),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", String)
 ], AppController.prototype, "getHello", null);
 AppController = __decorate([
-    (0, common_1.Controller)('v1'),
+    (0, common_1.Controller)(),
     __metadata("design:paramtypes", [app_service_1.AppService])
 ], AppController);
 exports.AppController = AppController;
