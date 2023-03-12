0347bdccd15edb64702c357ace1a6d12955374e3
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-03-12 16:45:10 +1300

    Revert "Update app.controller.ts"
    This reverts commit 8672581e0e608c77d8ce0a5cb95c7b3b76e178e0.

--- mobli-backend/src/app.controller.ts
+++ mobli-backend/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('v1')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-  @Get('test')
+  @Get()
   getHello(): string {
     return this.appService.getHello();
   }
