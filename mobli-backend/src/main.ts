002d3a6f21ce24cdb3cb202095941bc35a92cad6
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-03-12 14:36:58 +1300

    add api

--- mobli-backend/src/main.ts
+++ mobli-backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+
+const server = express();
+const adapter = new ExpressAdapter(server);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.init();
 }
+
 bootstrap();
+export const api = functions.https.onRequest(server);
