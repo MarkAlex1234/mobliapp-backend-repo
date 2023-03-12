8672581e0e608c77d8ce0a5cb95c7b3b76e178e0
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-03-12 14:38:24 +1300

    Update app.controller.ts

--- mobli-backend/src/app.controller.ts
+++ mobli-backend/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('test')
   getHello(): string {
     return this.appService.getHello();
   }
