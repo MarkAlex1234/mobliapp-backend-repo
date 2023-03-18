f14ee72d49dc0c38e172ca85d98be6c0965d0b08
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-03-18 13:55:03 +1300

    updates

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GetBusDetailsResponseInterface } from './common/interfaces/get-bus-details-response.interface';
 import { GetBusLocationResponseInterface } from './common/interfaces/get-bus-location-response.interface';
 import { PatchBusLocationResponseInterface } from './common/interfaces/patch-bus-location-response.interface';
 import * as admin from 'firebase-admin';
+import { HttpService } from '@nestjs/axios';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private httpService: HttpService,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -21,7 +25,7 @@ export class AppController {
   @Get(':busId/details')
   //Promise<GetBusDetailsResponseInterface>
   async getBusDetails() {
-    const snapshot = await admin.database().ref('/Bus').once('value');
+    const snapshot = await admin.database().ref('/Bus/:busId').once('value');
     const data = snapshot.val();
     return data;
   }
@@ -42,5 +46,29 @@ export class AppController {
   @Patch(':busId/location')
   updateBusLocation(): PatchBusLocationResponseInterface {
     return this.appService.getHello();
+  }
+
+  /**
+   *
+   */
+  @Get('route/:routeId')
+  async getBusOnTripByID(
+    @Param('routeId') routeId: string,
+  ): Promise<GetBusLocationResponseInterface> {
+    try {
+      const response = await this.httpService
+        .get(`https://api.at.govt.nz/gtfs/v3/routes/${routeId}`, {
+          headers: {
+            'Ocp-Apim-Subscription-Key': process.env.SUBSCRIPTION_KEY,
+          },
+        })
+        .toPromise();
+
+      console.log(response.data); // handle the response data here
+      return response.data.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
