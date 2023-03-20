081a7b25ac3e94070141af3f9588ef598ff25694
Lawrence WANG <1245304862@qq.com>
2023-03-20 20:27:47 +1300

    read and write functions updated

--- src/readandwrite.ts
+++ src/readandwrite.ts
@@ -1,6 +1,7 @@
 import firebase from "firebase-admin";
 import "firebase/database";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyDNOPHgGhibR_c7mAUsBW8fZPWXlVsWzc0",
   authDomain: "mobli-backend.firebaseapp.com",
@@ -16,15 +17,23 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-database.ref("Bus/:busId").on("value", (snapshot) => {
+//read data
+database.ref("/Bus/:busId").on("value", (snapshot) => {
     const data = snapshot.val();
     console.log(data);
 })
 
 database
-.ref("Bus/:busId")
+.ref("/Bus/:busId")
 .once("value")
 .then((snapshot) => {
     const data = snapshot.val();
     console.log(data);
+});
+
+//write data
+database.ref("/Bus/:busId").set({
+  busId: "busId",
+  busRoute: "busRoute",
+  busLocation: "busLocation",
 });
