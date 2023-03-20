67891dc05708788e20e728a6695380216f8ad079
Lawrence WANG <1245304862@qq.com>
2023-03-20 23:44:09 +1300

    read and write function updated

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,30 @@ import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
 import * as dotenv from 'dotenv';
 const serviceAccount = require('./../firebase.json');
+const firebaseConfig = require('./../firebaseConfig.json');
+import { getDatabase } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import { Logger } from '@nestjs/common/services';
+import { initial } from './readandwrite';
 
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const adminDatabase = getDatabase(firebaseApp);
 async function bootstrap() {
   dotenv.config();
-
+  //checking if the database is working.
+  if(adminDatabase){
+    Logger.log("admin database is initialized");
+  }else{
+   Logger.error("database not connected/initilized");
+  }
   //FIREBASE INIT
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 
+  initial();
   //NEST APP INIT
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
--- src/readandwrite.ts
+++ src/readandwrite.ts
@@ -1,50 +1,48 @@
-import firebase from "firebase/app";
-// import "firebase/database";
-import { getDatabase, ref, set } from "firebase/database";
+import firebase from "firebase-admin";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { Logger } from "@nestjs/common";
+import { adminDatabase } from "./main";
+import { read } from "fs";
+export const initial = () => {
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDNOPHgGhibR_c7mAUsBW8fZPWXlVsWzc0",
-  authDomain: "mobli-backend.firebaseapp.com",
-  databaseURL: "https://mobli-backend-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mobli-backend",
-  storageBucket: "mobli-backend.appspot.com",
-  messagingSenderId: "286542289831",
-  appId: "1:286542289831:web:59c64d9921c4c2b93af482",
-  measurementId: "G-QPKMWH0P07"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// let Ref = firebase.database().ref("/Bus");
-
-const database = getDatabase();
-
-function writeUserData(Id, Route, Location){
-  const db = getDatabase();
-  set(ref(db, '/Bus'), {
-    busId: Id,
-    busRoute: Route,
-    busLocation: Location
+if(adminDatabase !== null){
+  Logger.log("Acitvated [Read,Write]");
+}else{
+  Logger.log("Deactivated [Read,Write]");
+}
+const db = getDatabase();
+//Write data
+function writeUserData(id, route, location) {
+  set(ref(db, 'Bus/'), {
+    busId: id,
+    busRoute: route,
+    busLocation : location
   });
 }
+writeUserData("1245304862","ewrqwerqwfqwef", "XD");
+//Reading user data
+const busReference = ref(db, 'Bus/');
+onValue(busReference, (busData) => {
+  const data = busData.val();
+  Logger.log("BusID: " + data.busId);
+  Logger.log("BusRoute: " + data.busRoute);
+  Logger.log("BusLocation: " + data.busLocation); 
+});
 
-writeUserData("12342134", "Here to there", "nowhere");
-//write data
-// database.ref("/Bus/:busId").set({
-//   busId: "busId",
-//   busRoute: "busRoute",
-//   busLocation: "busLocation",
-// });
+function readuserdata(objectRef): string{
+  const busRef = ref(db, objectRef);
+  let tempdata;
+  onValue(busRef, (busData) => {
+  tempdata = busData.val();
+});
 
-//read data
-// database.ref("/Bus/:busId").on("value", (snapshot) => {
-//     const data = snapshot.val();
-//     console.log(data);
-// })
-// database
-// .ref("/Bus/:busId")
-// .once("value")
-// .then((snapshot) => {
-//     const data = snapshot.val();
-//     console.log(data);
-// });
+if(tempdata !== null){
+  return "BusID: " + tempdata.busId + "; " + "BusRoute: " + tempdata.busRoute + "; " +"BusLocation: " + tempdata.busLocation;
+}
+else{
+  return "";
+}
+}
+
+Logger.log(readuserdata('Bus/'));
+}
