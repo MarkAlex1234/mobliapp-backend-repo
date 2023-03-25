1747eb563187d527239827477253fbc0b13d1fac
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-03-26 09:55:39 +1300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,7 +20,7 @@ export class AppController {
   }
 
   /**
-   * Get a bus location
+   * Endpoint to get a bus location
    * @returns
    */
   @Get('bus/:busId/location')
@@ -29,7 +29,7 @@ export class AppController {
   }
 
   /**
-   * Update a bus' location in the database
+   * Endpoint to update a bus' location in the database
    * @returns
    */
   @Patch('bus/:busId/update-location')
