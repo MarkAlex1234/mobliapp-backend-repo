d8809aa6e6ca06af6d61fd9cb37bf70d14021071
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-02 14:25:10 +1200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BusModule } from './bus/bus.module';
 import { UsersModule } from './users/users.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [BusModule, UsersModule],
+  imports: [BusModule, UsersModule, AdminModule],
 })
 export class AppModule {}
