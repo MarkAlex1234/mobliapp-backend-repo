9bac22531429a84eec473f6ce42d78ceae2c594d
MarkAlex1234 <mark.alexander1314@gmail.com>
2023-04-25 14:00:39 +1200

    initial commit

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,7 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 
 @Controller('users')
 export class UsersContoller {
   constructor(private readonly usersService: UsersService) {}
+  /**
+   * Endpoint to get a user
+   */
+  @Get('user/:userId')
+  getUser(@Param('userId') userId: string) {
+    return this.usersService.getUser(userId);
+  }
+
+  /**
+   * Endpoint to get the nearest five buses to a user
+   */
+  @Get('user/:userId/nearest-buses')
+  getNearestBusesForUser(@Param('userId') userId: string) {
+    return this.usersService.getNearestBusesForUser(userId);
+  }
+
+  /**
+   * Endpoint to update a users location
+   */
+  @Patch('user/:userId/location')
+  updateUserLocation(@Param('userId') userId: string) {
+    return this.usersService.updateUserLocation(userId);
+  }
+
+  /**
+   * Endpoint for users to confirm the bus they are on
+   */
+  @Post('user/:userId/location')
+  setUserBusRide(@Param('userId') userId: string) {
+    return this.usersService.setUserBusRide(userId);
+  }
+
+  /**
+   * Endpoint to end a users ride
+   */
+  @Put('user/:userId/location')
+  endUserBusRide(@Param('userId') userId: string) {
+    return this.usersService.endUserBusRide(userId);
+  }
 }
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -4,4 +4,23 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   constructor(private httpService: HttpService) {}
+
+  getUser(userId: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  updateUserLocation(userId: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  getNearestBusesForUser(userId: string) {
+    throw new Error('Method not implemented.');
+  }
+
+  endUserBusRide(userId: string) {
+    throw new Error('Method not implemented.');
+  }
+  setUserBusRide(userId: string) {
+    throw new Error('Method not implemented.');
+  }
 }
